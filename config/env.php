1ca5f33cac7bbebe2185b4476d55fc343a033ba7
chandrapliquiloans <112621585+chandrapliquiloans@users.noreply.github.com>
2024-02-14 10:18:54 +0530

    AppAsset folder issue

--- config/env.php
+++ config/env.php
@@ -1,6 +1,6 @@
 <?php
 defined ("SITE_LOGO") or define("SITE_LOGO","./dist/img/logo.png");
-defined ("SITE_NAME") or define("SITE_NAME","UNICRANE");
+defined ("SITE_NAME") or define("SITE_NAME","Stores");
 defined ("SITE_ADDRESS") or define("SITE_ADDRESS","795 Folsom Ave, Suite 600, San Francisco, CA 94107");
 defined ("SITE_PHONE") or define("SITE_PHONE","(804) 123-5432");
 defined ("SITE_EMAIL") or define("SITE_EMAIL","info@almasaeedstudio.com");
